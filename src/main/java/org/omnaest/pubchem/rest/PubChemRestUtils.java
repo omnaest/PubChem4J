894ebe2445501a7794ffeaaefe31d16a62a7c797
omnaest <omnaest@web.de>
2021-10-17 01:34:48 +0200

    added support to fetch title and descriptions

--- src/main/java/org/omnaest/pubchem/rest/PubChemRestUtils.java
+++ src/main/java/org/omnaest/pubchem/rest/PubChemRestUtils.java
@@ -1,17 +1,26 @@
 package org.omnaest.pubchem.rest;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.omnaest.pubchem.rest.domain.Synonyms;
 import org.omnaest.utils.CacheUtils;
 import org.omnaest.utils.JSONHelper;
+import org.omnaest.utils.StreamUtils;
 import org.omnaest.utils.cache.Cache;
 import org.omnaest.utils.rest.client.RestClient;
 import org.omnaest.utils.rest.client.RestHelper.RESTAccessExeption;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.JsonNode;
 
 public class PubChemRestUtils
@@ -53,19 +62,102 @@ public class PubChemRestUtils
                                        .build();
                 try
                 {
-                    LOG.info("Fetching pubchem synonyms for " + compoundName);
-                    JsonNode response = restClient.requestGet(url, JsonNode.class);
-                    JsonNode informationArray = response.findPath("Information");
-                    return JSONHelper.toArrayNode(informationArray)
-                                     .map(arrayNode -> arrayNode.get(0))
-                                     .map(JSONHelper.toObjectWithTypeMapper(Synonyms.class));
+                    LOG.debug("Fetching pubchem synonyms for " + compoundName);
+                    return restClient.request()
+                                     .toUrl(url)
+                                     .getAnd(JsonNode.class)
+                                     .handleStatusCode(404, holder -> null)
+                                     .asOptional()
+                                     .map(response -> response.findPath("Information"))
+                                     .flatMap(informationArray -> JSONHelper.toArrayNode(informationArray)
+                                                                            .map(arrayNode -> arrayNode.get(0))
+                                                                            .map(JSONHelper.toObjectWithTypeMapper(Synonyms.class)));
                 }
                 catch (RESTAccessExeption e)
                 {
                     if (e.getStatusCode() == 404)
                     {
-                        LOG.info("Unable to find chemical synonym for " + compoundName);
+                        LOG.error("Unable to find chemical synonym for " + compoundName);
                         return Optional.empty();
+                    }
+                    else
+                    {
+                        throw e;
+                    }
+                }
+            }
+
+            @Override
+            public Optional<String> fetchTitle(String cid)
+            {
+                return this.fetchDescriptions(cid)
+                           .filter(Description::hasTitle)
+                           .map(Description::getTitle)
+                           .findFirst();
+            }
+
+            @Override
+            public Map<String, String> fetchTitles(String... cids)
+            {
+                return this.fetchTitles(Arrays.asList(cids));
+            }
+
+            @Override
+            public Map<String, String> fetchTitles(Collection<String> cids)
+            {
+                return this.fetchDescriptions(cids)
+                           .filter(Description::hasTitle)
+                           .collect(Collectors.toMap(Description::getCid, Description::getTitle));
+            }
+
+            @Override
+            public Stream<Description> fetchDescriptions(String... cid)
+            {
+                return this.fetchDescriptions(Arrays.asList(cid));
+            }
+
+            @Override
+            public Stream<Description> fetchDescriptions(Collection<String> cids)
+            {
+                try
+                {
+                    return StreamUtils.framedNonNullAsList(10, Optional.ofNullable(cids)
+                                                                       .orElse(Collections.emptyList())
+                                                                       .stream()
+                                                                       .distinct())
+                                      .flatMap(cidBatch ->
+                                      {
+
+                                          RestClient restClient = this.newRestClient();
+
+                                          String url = RestClient.urlBuilder()
+                                                                 .setBaseUrl(this.baseUrl)
+                                                                 .addPathToken("compound")
+                                                                 .addPathToken("cid")
+                                                                 .addPathToken(cidBatch.stream()
+                                                                                       .collect(Collectors.joining(",")))
+                                                                 .addPathToken("description")
+                                                                 .addPathToken("JSON")
+                                                                 .build();
+                                          LOG.debug("Fetching pubchem descriptions for " + cidBatch);
+                                          return restClient.request()
+                                                           .toUrl(url)
+                                                           .getAnd(JsonNode.class)
+                                                           .handleStatusCode(400, holder -> null)
+                                                           .asOptional()
+                                                           .map(response -> response.findPath("Information"))
+                                                           .flatMap(informationArray -> JSONHelper.toArrayNode(informationArray)
+                                                                                                  .map(arrayNode -> StreamUtils.fromIterator(arrayNode.iterator())))
+                                                           .orElse(Stream.empty())
+                                                           .map(JSONHelper.toObjectWithTypeMapper(Description.class));
+                                      });
+                }
+                catch (RESTAccessExeption e)
+                {
+                    if (e.getStatusCode() == 404)
+                    {
+                        LOG.error("Unable to find chemical descriptions for " + cids);
+                        return Stream.empty();
                     }
                     else
                     {
@@ -78,7 +170,7 @@ public class PubChemRestUtils
             {
                 return RestClient.newJSONRestClient()
                                  .withCache(this.cache)
-                                 .withRetry(5, 12, TimeUnit.SECONDS);
+                                 .withRetry(10, 12, TimeUnit.SECONDS);
             }
 
         };
@@ -91,6 +183,56 @@ public class PubChemRestUtils
         public PubChemRestAccessor withCache(Cache cache);
 
         public PubChemRestAccessor withLocalCache();
+
+        public Optional<String> fetchTitle(String cid);
+
+        public Map<String, String> fetchTitles(String... cids);
+
+        public Map<String, String> fetchTitles(Collection<String> cids);
+
+        public Stream<Description> fetchDescriptions(Collection<String> cids);
+
+        public Stream<Description> fetchDescriptions(String... cid);
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Description
+    {
+        @JsonProperty("CID")
+        private String cid;
+
+        @JsonProperty("Title")
+        private String title;
+
+        @JsonProperty("Description")
+        private String description;
+
+        public boolean hasTitle()
+        {
+            return this.title != null;
+        }
+
+        public String getCid()
+        {
+            return this.cid;
+        }
+
+        public String getTitle()
+        {
+            return this.title;
+        }
+
+        public String getDescription()
+        {
+            return this.description;
+        }
+
+        @Override
+        public String toString()
+        {
+            return "Description [cid=" + this.cid + ", title=" + this.title + ", description=" + this.description + "]";
+        }
+
     }
 
 }
--- src/test/java/org/omnaest/pubchem/rest/PubChemRestUtilsTest.java
+++ src/test/java/org/omnaest/pubchem/rest/PubChemRestUtilsTest.java
@@ -1,9 +1,11 @@
 package org.omnaest.pubchem.rest;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
+import java.util.Map;
 
 import org.junit.Ignore;
 import org.junit.Test;
@@ -25,5 +27,37 @@ public class PubChemRestUtilsTest
         assertEquals(6305, synonyms.getCid());
         assertTrue(synonyms.getSynonyms()
                            .containsAll(Arrays.asList("L-tryptophan", "Indole-3-alanine")));
+    }
+
+    @Test
+    @Ignore
+    public void testNonExisting() throws Exception
+    {
+        assertFalse(PubChemRestUtils.newInstance()
+                                    .withLocalCache()
+                                    .fetchSynonyms("non-existing")
+                                    .isPresent());
+    }
+
+    @Test
+    @Ignore
+    public void testTitle() throws Exception
+    {
+        Map<String, String> result = PubChemRestUtils.newInstance()
+                                                     //                                                .withLocalCache()
+                                                     .fetchTitles("222", "280", "5957", "962", "1061", "6022", "57339278", "3080745", "977", "70678894",
+                                                                  "70678937");
+        assertEquals(11, result.size());
+        assertEquals("3-Deoxy-D-erythro-hex-2-ulosonic acid 6-phosphate", result.get("3080745"));
+        assertEquals("Ammonia", result.get("222"));
+        assertEquals("Adenosine-5'-diphosphate", result.get("6022"));
+        assertEquals("Water", result.get("962"));
+        assertEquals("1-Deoxypentalenate", result.get("70678894"));
+        assertEquals("Oxygen", result.get("977"));
+        assertEquals("(+)-6-Endo-hydroxycamphor", result.get("57339278"));
+        assertEquals("Carbon dioxide", result.get("280"));
+        assertEquals("Pentalenate", result.get("70678937"));
+        assertEquals("Phosphate", result.get("1061"));
+        assertEquals("Adenosine-5'-triphosphate", result.get("5957"));
     }
 }
